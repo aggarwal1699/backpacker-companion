8b719a28e05d112de70bd40f56db1be2b9f38d05
Shubham.Aggarwal <shubham.aggarwal@dpworld.com>
2026-08-21 00:09:08 +0530

    chore: initialize Spring Boot application

---
+++ src/main/java/com/backpacker/BackpackerCompanionApplication.java
@@ -0,0 +1,13 @@
+package com.backpacker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackpackerCompanionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackpackerCompanionApplication.class, args);
+	}
+
+}
